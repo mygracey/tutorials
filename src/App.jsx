954821e70982296d131a60f35524000b345cece3
grace <graceogwe@gmail.com>
2024-10-29 13:18:58 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+
+function App(){
+  
+
+  return(
+    <div className="app">
+
+    <div className="home-section"></div>
+
+    <div className="about-section"></div>
+
+    <div className="services-section"></div>
+
+    <div className="contact-section"></div>
+
+
+    </div>
+  )
+
+
+}
+
+export default App
